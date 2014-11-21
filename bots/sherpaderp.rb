766fc113fbe9816c7aac9417efa7c270fe27486e
Sherry Shao <sshao0123@gmail.com>
2014-11-20 16:49:54 -0800

    More logic; unlocking beast mode; circumference calculation pending

--- bots/sherpaderp.rb
+++ bots/sherpaderp.rb
@@ -6,16 +6,17 @@ class Sherpaderp < RTanque::Bot::Brain
   NAME = 'Sherpaderp'
   include RTanque::Bot::BrainHelper
 
-  TURRET_SHORT_DIST_RANGE = RTanque::Heading::ONE_DEGREE * 5.0
-  TURRET_LONG_DIST_RANGE = RTanque::Heading::ONE_DEGREE * 1.0
+  TURRET_FIRE_RANGE = RTanque::Heading::ONE_DEGREE * 5.0
 
   def tick!
+    @clockwise ||= true
     if (derp = self.get_derp)
       self.beast_mode(derp)
     else
       self.find_derp
     end
 
+    self.rotate_spiral
     self.hodor
   end
 
@@ -27,13 +28,25 @@ class Sherpaderp < RTanque::Bot::Brain
   # Notes:
   # DFD = Distance From Derp
   def beast_mode(derp)
-    desired_dfd = ([self.arena.width, self.arena.height].min / 2) * self.health_factor
+    @derp_last_seen = derp.position
+
+    largest_radius = [self.arena.width, self.arena.height].min / 2
+    desired_dfd = largest_radius * self.health_factor
     dfd_delta = (desired_dfd - derp.distance).abs
 
-    if dfd_delta > 20
-      
+    command.turret_heading = derp.heading
+    command.radar_heading = derp.heading
+
+    if (derp.heading.delta(sensors.turret_heading)).abs < TURRET_FIRE_RANGE
+      command.fire(dtd > 200 ? MAX_FIRE_POWER : MIN_FIRE_POWER)
+    end
+
+    if dfd_delta <= 20 # we are at desired distance away
+      command.heading = self.angle_to_next_dot_on_spiral(derp.position, dfd_delta)
+      command.speed = MAX_BOT_SPEED * (1 - (derp.distance / largest_radius))
     else
-      
+      command.heading = desired_dfd > derp.distance ? self.angle_from_derp(derp) : self.angle_from_derp(derp) + 180
+      command.speed = MAX_BOT_SPEED
     end
 
   end
@@ -60,6 +73,23 @@ class Sherpaderp < RTanque::Bot::Brain
     if @facing
       command.heading = @facing
       command.turret_heading = @facing
+    end
+  end
+
+  # get angle from derp
+  def angle_from_derp(derp)
+    Math.atan2(self.position.y - derp.position.y, self.position.x - derp.position.x) * 180 / Math::PI
+  end
+
+  # get next position on spiral
+  def angle_to_next_dot_on_spiral(center, radius)
+    
+  end
+
+  # rotate spiral
+  def rotate_spiral
+    at_tick_interval(5000) do
+      @clockwise = !@clockwise
     end
   end
 
